4c76841353f37865edb5a11fb84ba8ea2b7eaf09
Damien V <100078122+DamienVaurs@users.noreply.github.com>
2022-02-25 09:40:27 +0000

    Intégration de tweepy

--- main.py
+++ main.py
@@ -6,10 +6,21 @@ from discord.utils import get
 from discord.ext import commands, tasks
 from discord.ext.commands import has_permissions,  CheckFailure, check
 
+import tweepy
+
+my_secret1 = os.environ['TwitterToken']
+auth = tweepy.OAuth2BearerHandler(my_secret1)
+api = tweepy.API(auth)
+
+
+username='LMPositif'
+tweets_list= api.user_timeline(username, count=1, tweet_mode='extended')
+tweet= tweet_list[0]
+print(tweet.full_text)
 
 client = discord.Client()
 
-client = commands.Bot(command_prefix = '!')  here
+client = commands.Bot(command_prefix = '!') 
 
 @client.event
 async def on_ready():
